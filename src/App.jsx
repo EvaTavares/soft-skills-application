e0cecb391e38ebe890849b6f30d7138b5b33f12d
Eva Tavares <eva.programadora@gmail.com>
2023-07-03 22:48:56 -0300

    add novas rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-// import EvaPage from "./pages/EvaPage";
-// import JonasPage from "./pages/JonasPage";
-// import RobertoPage from "./pages/RobertoPage";
+import EvaPage from "./pages/EvaPage";
+import JonasPage from "./pages/JonasPage";
+import RobertoPage from "./pages/RobertoPage";
+import MarselPage from "./pages/MarselPage copy";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        {/* <Route path="/evaPage" element={<EvaPage />} />
+        <Route path="/evaPage" element={<EvaPage />} />
         <Route path="/jonasPage" element={<JonasPage />} />
-        <Route path="/robertoPage" element={<RobertoPage />} /> */}
+        <Route path="/robertoPage" element={<RobertoPage />} />
+        <Route path="/marselPage" element={<MarselPage />} />
       </Routes>
     </BrowserRouter>
   );
