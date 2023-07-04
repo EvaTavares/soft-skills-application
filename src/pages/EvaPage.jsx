eb7bc929ce37dcafe88ff7dddd441eb88361264f
Eva Tavares <eva.programadora@gmail.com>
2023-07-04 17:04:36 -0300

    pagina Eva

--- src/pages/EvaPage.jsx
+++ src/pages/EvaPage.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import evaimg from "../assets/images/eva-avatar.jpeg";
 
 const EvaPage = () => {
   return (
     <div>
-      <h1 style={{ color: "white" }}>Minha Página EVA</h1>
-      {/* Coloque o conteúdo da página aqui */}
+      <div className="container2">
+        <div className="card">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <div className="centro">
+            <img className="img" src={evaimg} />
+          </div>
+        </div>
+
+        <div className="skills">
+          <h1 className="heading">Eva Tavares</h1>
+          <ol>
+            <li className="text-skills">softs skills</li>
+            <li className="text-skills">softs skills</li>
+            <li className="text-skills">softs skills</li>
+          </ol>
+        </div>
+      </div>
     </div>
   );
 };
