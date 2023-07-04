c387c4f0eec98c04afb5516c2b331d425740dd6b
Eva Tavares <eva.programadora@gmail.com>
2023-07-04 17:04:43 -0300

    Pagina Jonas

--- src/pages/JonasPage.jsx
+++ src/pages/JonasPage.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import jonasImg from "../assets/images/jonas-avatar.jpeg";
 
 const JonasPage = () => {
   return (
     <div>
-      <h1 style={{ color: "white" }}>Minha Página Jonas</h1>
-      {/* Coloque o conteúdo da página aqui */}
+      <div className="container2">
+        <div className="card">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <div className="centro">
+            <img className="img" src={jonasImg} />
+          </div>
+        </div>
+
+        <div className="skills">
+          <h1 className="heading">Jonas Silva</h1>
+          <ol>
+            <li className="text-skills">softs skills</li>
+            <li className="text-skills">softs skills</li>
+            <li className="text-skills">softs skills</li>
+          </ol>
+        </div>
+      </div>
     </div>
   );
 };
